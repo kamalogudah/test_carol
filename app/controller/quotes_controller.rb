8cb44b7e041232b4946c8fb186b761a09dd78e77
kamalogudah <mcpaul2058@gmail.com>
2018-05-17 00:09:16 +0300

    Controller action and routing

---
+++ app/controller/quotes_controller.rb
@@ -0,0 +1,6 @@
+class QuotesController < Carol::Controller
+  def a_quote
+    "There is nothing either good or bad " +
+    "but thinking makes it so."
+  end
+end
--- config/application.rb
+++ config/application.rb
@@ -1,7 +1,12 @@
 require "carol"
+$LOAD_PATH << File.join(File.dirname(__FILE__),
+                        "..", "app",
+                        "controller")
+
+require 'quotes_controller'
 
 module TestCarol
   class Application < Carol::Application
-    
+
   end
 end
