5f1484f8d9ef1146ed6de63e3e4cfab7928a1c42
kamalogudah <mcpaul2058@gmail.com>
2018-05-27 15:48:03 +0300

    Seek and find

--- sqlite_test.rb
+++ sqlite_test.rb
@@ -2,9 +2,11 @@ require "sqlite3"
 require "carol/sqlite_model"
 class MyTable < Carol::Model::SQLite; end
 STDERR.puts MyTable.schema.inspect
-
 # Create row
-mt = MyTable.create "title" => "It happened!",
-"posted" => 1, "body" => "It did!"
-mt = MyTable.create "title" => "I saw it!"
+mt = MyTable.create "title" => "I saw it again!"
 puts "Count: #{MyTable.count}"
+top_id = mt["id"].to_i
+(1..top_id).each do |id|
+  mt_id = MyTable.find(id)
+  puts "Found title #{mt_id["title"]}."
+end
