cfcb43d0659095b80ca5926b0342a7b5ad9259d6
kamalogudah <mcpaul2058@gmail.com>
2018-05-28 11:25:39 +0300

    Saving objects in table

--- sqlite_test.rb
+++ sqlite_test.rb
@@ -4,9 +4,14 @@ class MyTable < Carol::Model::SQLite; end
 STDERR.puts MyTable.schema.inspect
 # Create row
 mt = MyTable.create "title" => "I saw it again!"
+mt["title"] = "I really did not though!"
+mt.save!
+mt2 = MyTable.find mt["id"]
+puts "Title: #{mt2["title"]}"
 puts "Count: #{MyTable.count}"
 top_id = mt["id"].to_i
 (1..top_id).each do |id|
   mt_id = MyTable.find(id)
   puts "Found title #{mt_id["title"]}."
 end
+
